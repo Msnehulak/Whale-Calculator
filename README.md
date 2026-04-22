03bbdba9325d549abdc6bf4ecfc5da16543d50ca
Msnehulak <mxmhrmn@gmail.com>
2026-04-22 14:12:12 +0200

    "spend" is now float, update format README.md

--- README.md
+++ README.md
@@ -3,10 +3,17 @@ This is a Calculator what is a maximu for GI whale spend on game with worst luck
 
 ## Total Spend
 all C6 characters   190002.21 usd
+
 all R5 weapons      60318.16 usd
+
 Welkin Moon         334.33 usd
+
 Battle Pass         479.52 usd
+
 Battle Pass levl up 5654.83 usd
+
 Refil Resin         7979.59 usd
+
+---
 
 total               264768.64 usd
--- update_readme.py
+++ update_readme.py
@@ -7,16 +7,30 @@ def write_readme(markdown):
 wc = whale_calculator.whalecalculator()
 data = wc.get_row_data()
 
+# float update
+for key, value in data.items():
+    if isinstance(value, dict) and "spend" in value:
+        data[key]["spend"] = float(value["spend"])
+
+data["total_spend"] = float(data["total_spend"])
+
 markdown = f"""# Genshin Impact - Whale Calculator
 This is a Calculator what is a maximu for GI whale spend on game with worst luck. 
 
 ## Total Spend
 all C6 characters   {data["Characters"]["spend"]:.2f} usd
+
 all R5 weapons      {data["Weapons"]["spend"]:.2f} usd
+
 Welkin Moon         {data["Welkin_Moon"]["spend"]:.2f} usd
+
 Battle Pass         {data["BP"]["spend"]:.2f} usd
+
 Battle Pass levl up {data["BP_LV_UP"]["spend"]:.2f} usd
+
 Refil Resin         {data["resin_refill"]["spend"]:.2f} usd
+
+---
 
 total               {data["total_spend"]:.2f} usd
 """
