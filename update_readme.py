54b31a2bdfa6bf2a78a877b97734ea0929f991a5
Msnehulak <mxmhrmn@gmail.com>
2026-04-22 14:01:05 +0200

    add daily update README.md
    [add]
     -  daily update README.md
     -  update_readme.py
    
    [fix]
     - wale_calculator.py to whale_calculator.py (add mising 'h')

---
+++ update_readme.py
@@ -0,0 +1,24 @@
+import whale_calculator
+
+def write_readme(markdown):
+    with open("README.md", "w", encoding="utf-8") as f:
+        f.write(markdown)
+
+wc = whale_calculator.whalecalculator()
+data = wc.get_row_data()
+
+markdown = f"""# Genshin Impact - Whale Calculator
+This is a Calculator what is a maximu for GI whale spend on game with worst luck. 
+
+## Total Spend
+all C6 characters   {data["Characters"]["spend"]:.2f} usd
+all R5 weapons      {data["Weapons"]["spend"]:.2f} usd
+Welkin Moon         {data["Welkin_Moon"]["spend"]:.2f} usd
+Battle Pass         {data["BP"]["spend"]:.2f} usd
+Battle Pass levl up {data["BP_LV_UP"]["spend"]:.2f} usd
+Refil Resin         {data["resin_refill"]["spend"]:.2f} usd
+
+total               {data["total_spend"]:.2f} usd
+"""
+
+write_readme(markdown)
---
+++ whale_calculator.py
@@ -0,0 +1,162 @@
+from datetime import date
+import requests
+
+class whalecalculator:
+    def __init__(self):
+        self.prices = {
+            "welkin": 4.99,
+            "battle_pass": 9.99,
+            "crystal": [
+                [60, 0.99],
+                [300, 4.99],
+                [980, 14.99],
+                [1980, 29.99],
+                [3280, 49.99],
+                [6480, 99.99]
+            ]
+        }
+        self.primo = {
+            "bplvup": 150,
+            "refil_resin": 50,
+            "pull": 160
+        }
+        self.total_spend = 0
+        
+
+        self.release_date = date(2020, 9, 28)
+        self.today_date = date.today()
+        self.days_from_releas = self.today_date - self.release_date
+        self.days_from_releas = self.days_from_releas.days
+        self.versin_released = self.days_from_releas // (7 * 6) 
+
+        self.Characters()
+        self.Weapons()
+        self.Welkin_Moon()
+        self.Batle_pass()
+        self.Batle_pass_levl_up()
+        self.Daly_resin_refill()
+
+    def Characters(self):
+        self.char_standard5_list = ["Jean", "Diluc", "Qiqi", "Mona", "Keqing", "Tighnari", "Dehya", "Mizuki"]
+        self.char_standard5_count = len(self.char_standard5_list) 
+        self.char_limited5char_list = self.get_limited_character_count()
+        self.char_limited5_count = len(self.char_limited5char_list)
+
+        self.char_pulls_one_copy = 180
+        self.char_pulls_C6 = self.char_pulls_one_copy * 7
+        self.char_total_pulls = self.char_pulls_C6 * self.char_limited5_count
+        self.char_total_primo = self.char_total_pulls * self.primo["pull"]
+        self.char_spend = self.primo_to_usd(self.char_total_primo)
+
+        self.total_spend += self.char_spend
+    
+    def Weapons(self):
+        self.wpn_count = self.char_limited5_count
+        self.wpn_pulls_one_copy = 80
+        self.wpn_pulls_R5 = self.wpn_pulls_one_copy * 5
+        self.wpn_total_pulls = self.wpn_pulls_R5 * self.wpn_count
+        self.wpn_total_primo = self.wpn_total_pulls * self.primo["pull"]
+        self.wpn_spend = self.primo_to_usd(self.wpn_total_primo)
+
+        self.total_spend += self.wpn_spend
+
+    def Welkin_Moon(self):
+        self.welkin_moon_owned = self.days_from_releas // 30 
+        self.welkin_moon_spend = self.welkin_moon_owned * self.prices["welkin"]
+        
+        self.total_spend += self.welkin_moon_spend
+
+    def Batle_pass(self):
+        self.BP_owned = self.versin_released
+        self.BP_spend = self.BP_owned * self.prices["battle_pass"]
+        
+        self.total_spend += self.BP_spend
+
+    def Batle_pass_levl_up(self):
+        self.BP_LV_UP_count = self.BP_owned * 50
+        self.BP_LV_UP_spend_primo = self.BP_LV_UP_count * self.primo["bplvup"]
+        self.BP_LV_UP_spend = self.primo_to_usd(self.BP_LV_UP_spend_primo)
+
+        self.total_spend += self.BP_LV_UP_spend 
+
+    def Daly_resin_refill(self):
+        self.resin_refill_limit = 5
+        self.resin_refil_count = self.resin_refill_limit * self.days_from_releas
+        self.resin_refil_spend_primo = self.resin_refil_count * self.primo["refil_resin"]
+        self.resin_refil_spend = self.primo_to_usd(self.resin_refil_spend_primo)
+
+        self.total_spend += self.resin_refil_spend
+
+    def get_limited_character_count(self):
+            # requests
+        url = "https://genshin-db-api.vercel.app/api/characters?query=5&matchCategories=true"
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            characters = list(response.json())
+        else:
+            print("error, API don't work")
+            return []
+        
+            # Filter standard 5*
+        limited5char = []
+        for char in characters:
+            if char in self.char_standard5_list:
+                pass
+            else:
+                limited5char.append(char)
+        return limited5char
+
+    def primo_to_usd(self, primo):
+        bundles = self.prices["crystal"]
+
+        costs_per_crystal = []
+        for crys, price in bundles:
+            costs_per_crystal.append(price / crys)
+        
+        average_cost_per_crystal = sum(costs_per_crystal) / len(costs_per_crystal)
+        
+        return round(primo * average_cost_per_crystal, 2)
+
+    def get_row_data(self):
+        return {
+            "Characters": {
+                "spend": self.char_spend
+            },
+            "Weapons": {
+                "spend": self.wpn_spend
+            },
+            "Welkin_Moon": {
+                "spend": self.welkin_moon_spend
+            },
+            "BP": {
+                "spend": self.BP_spend
+            },
+            "BP_LV_UP": {
+                "spend": self.BP_LV_UP_spend
+            },
+            "resin_refill": {
+                "spend": self.resin_refil_spend   
+            },
+            "total_spend": self.total_spend
+        }
+
+    def main(self):
+        print(f"""
+{"="*40}
+            Total Spend
+Welkin Moon         {self.welkin_moon_spend:.2f} usd
+Battle Pass         {self.BP_spend:.2f} usd
+Battle Pass levl up {self.BP_LV_UP_spend:.2f} usd
+Refil Resin         {self.resin_refil_spend:.2f} usd
+all C6 characters   {self.char_spend:.2f} usd
+all R5 weapons      {self.wpn_spend:.2f} usd 
+
+total               {self.total_spend:.2f} usd
+{"="*40}
+""")
+
+
+if __name__ == "__main__":    
+    wc = whalecalculator()
+    wc.main()
